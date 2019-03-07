a006861c5c97c963600c539f599a0404ec78cbe2
Alejandro-Sierra <alexsierra1598@gmail.com>
2019-03-07 15:23:15 -0500

    edit plant images

--- src/components/EditPlant.js
+++ src/components/EditPlant.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import { Field, reduxForm } from "redux-form";
+import { Field, reduxForm, change } from "redux-form";
 import { connect } from 'react-redux';
-import { editPlant, getPlants } from '../actions'
+import { editPlant, getPlants } from '../actions';
+import axios from 'axios';
+import ImageList from './imageList';
 
 class EditPlant extends Component {
     state = {
-        userPlant: null
+        searchedImage: '',
+        imageList: [],
+        selectedImage: null
     }
+
 
     //fetch the data on page reload!!!
     componentWillMount = () => {
@@ -33,6 +38,26 @@ class EditPlant extends Component {
         )
     }
 
+    onImageSubmit = () => {
+
+        const image = this.state.searchedImage;
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: image },
+            headers: {
+                Authorization: 'Client-ID c85af5822bfc242f5f7f8c34f93eb58f86961cf3c6983b97d24fc2e417f05d5b'
+            }
+        }).then(res => {
+            this.setState({ imageList: res.data.results })
+            console.log(res.data.results);
+
+        })
+    }
+
+    onSelectedImage = (data) => {
+        this.setState({ selectedImage: data });
+        this.props.dispatch(change('createPlant', 'image', this.state.selectedImage));
+    }
+
     submitForm = (formValues) => {
         this.props.editPlant(this.props.match.params.id, formValues)
 
@@ -40,47 +65,63 @@ class EditPlant extends Component {
 
     render() {
         return (
-            <div>
-                Edit Plant
+            <div className="ui container">
+                Edit plant
+                <form onSubmit={e => e.preventDefault()}
+                    className="ui form"
+                >
+                    <div className="field">
+                        <label>Plant name</label>
+                        <Field name="name" component={this.renderInput}
+                            placeholder="Enter name of plant"
+                        />
 
-               <div className="ui container">
-                    <form onSubmit={this.props.handleSubmit(this.submitForm)}
-                        className="ui form">
-                        <div className="field">
-                            <label>Plant name</label>
-                            <Field name="name" component={this.renderInput}
-                                placeholder="Enter name of plant"
-                            />
+                    </div>
 
+                    <div className="field">
+                        <label>Plant description</label>
+                        <Field name="description" component={this.renderInput}
+                            placeholder="Enter description of plant"
+                        />
+
+                    </div>
+
+
+                    <div className="field">
+                        <div className="ui action input">
+                            <input onChange={e => this.setState({ searchedImage: e.target.value })} />
+                            <button
+                                onClick={this.onImageSubmit}
+                                className="ui olive button">Search Image</button>
                         </div>
 
-                        <div className="field">
-                            <label>Plant description</label>
-                            <Field name="description" component={this.renderInput}
-                                placeholder="Enter description of plant"
-                            />
+                        <ImageList
+                            images={this.state.imageList}
+                            onSelectedImage={this.onSelectedImage}
+                        />
 
+
+                        <div className="ui transparent input">
+                            <Field name="image" component={this.renderInput} />
                         </div>
 
-                        <div className="field">
-                            <label>Select image</label>
-                            <Field name="image" type="file" component={this.renderInput} />
-                        </div>
 
-                        <div className="field">
-                            <label>Next Water Date</label>
-                            <Field name="date" type="date" component={this.renderInput} />
-                        </div>
+                    </div>
 
-                        <div className="field">
-                            <label>Plant notes</label>
-                            <Field component={this.renderTextArea} name="notes" rows="4" placeholder="write any notes in here. (describe anything that you find fascinating)" />
+                    <div className="field">
+                        <label>Next Water Date</label>
+                        <Field name="date" type="date" onChange={this.handleFile} component={this.renderInput} />
+                    </div>
 
-                        </div>
+                    <div className="field">
+                        <label>Plant notes</label>
+                        <Field component={this.renderTextArea} name="notes" rows="4" placeholder="write any notes in here. (describe anything that you find fascinating)" />
 
-                        <button className="ui olive button" type="submit">Save plant</button>
-                    </form>
-                </div>
+                    </div>
+
+                </form>
+
+                <button className="ui olive button" onClick={this.props.handleSubmit(this.submitForm)}>Save plant</button>
             </div>
         )
     }
